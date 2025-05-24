4578d28984e33af8c41ef40f049f5852a5d8587b
SergioZepol <sergioozepol@gmail.com>
2025-05-24 13:06:08 +0200

    Relay conectado (por implementar spawn con relay)
    Organización: SergioZepol.
    Proyecto: EMZERO

--- Assets/Scripts/Nuevos/UIManager.cs
+++ Assets/Scripts/Nuevos/UIManager.cs
@@ -1,8 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay.Models;
+using Unity.Services.Relay;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
@@ -35,7 +38,6 @@ public class UIManager : MonoBehaviour
     {
         if (GUILayout.Button("Host")) StartHost();
         if (GUILayout.Button("Client")) StartClient();
-        if (GUILayout.Button("Server")) StartServer();
     }
 
     void StartHost()
@@ -43,15 +45,48 @@ public class UIManager : MonoBehaviour
         NetworkManager.Singleton.StartHost();
     }
 
+    /*
+        private async void StartHost()
+    {
+        await UnityServices.InitializeAsync();
+
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+        joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+        TextEditor te = new TextEditor();
+        te.text = joinCode;
+        te.SelectAll();
+        te.Copy();
+
+        NetworkManager.Singleton.StartHost();
+    }
+     */
+
     void StartClient()
     {
         NetworkManager.Singleton.StartClient();
     }
 
-    void StartServer()
+    /*
+        private async void StartClient()
     {
-        NetworkManager.Singleton.StartServer();
+        await UnityServices.InitializeAsync();
+        if (!AuthenticationService.Instance.IsSignedIn)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        }
+
+        var joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
+        NetworkManager.Singleton.StartClient();
     }
+    */
 
     void StatusLabels()
     {
